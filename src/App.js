19cba5b850525e58e0d86bc0358990438c5e6505
Eric Njanga <njanga.webdev@gmail.com>
2018-04-11 14:16:35 -0400

    Integrated global layout into basic components

--- src/App.js
+++ src/App.js
@@ -17,34 +17,73 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <header className="App-header">
-             
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <main>
-            <MainTabs />
-            <Sidebar />
-
-
-            <div className="center">
-              <Route exact={true} path="/" render={()=>(
-                <h1>Welcome</h1>
-              )} />
-              
-              <Route path="/login" component={Login} />
-              <Route path="/register" component={Register} />
-              <Route path="/around-us" component={Aroundus} /> 
-              <Route path="/profile" component={Profile} /> 
-              <Route path="/privacy" component={Privacy} /> 
-            </div>
-          </main>
-        </div>
+      <Router> 
+        <div className="mdl-layout mdl-js-layout mdl-layout--no-drawer-button">
+          <DrawerNav />
+          <main className="mdl-layout__content"> 
+            <section className="mdl-tabs mdl-js-tabs mdl-js-ripple-effect">
+              <MainHeader /> 
+              <main className="main-content">
+                <Sidebar />
+                <div className="center">
+                  <Route exact={true} path="/" render={()=>(
+                    <h1>Welcome</h1>
+                  )} />
+                  
+                  <Route path="/login" component={Login} />
+                  <Route path="/register" component={Register} />
+                  <Route path="/around-us" component={Aroundus} /> 
+                  <Route path="/profile" component={Profile} /> 
+                  <Route path="/privacy" component={Privacy} /> 
+                </div>
+              </main> 
+            </section>{/* mdl-tabs */}
+          </main>{/* mdl-layout__content */}
+        </div>{/* mdl-layout */}
       </Router>
     );
   }
-}
+}//[end] App
+
+
+const DrawerNav = () => {
+  return(
+    <div className="mdl-layout__drawer">
+      <div className="mdl-layout-title">
+        <img src="./images/rsz_avatar3.jpg" alt="" className="fanci-avatar fanci-avatar__medium" />
+        <h3>User Name</h3>
+        <h4>
+          email@address.com
+        </h4>
+        <ul className="mdl-layout-info">
+          <li>
+            <span>14</span> Fancies
+          </li>
+          <li>
+            <span>37</span> Participants
+          </li>
+        </ul> 
+      </div>
+
+      <hr />
+
+      <nav className="mdl-navigation">
+        <Link to={`/profile`} className="mdl-navigation__link mdl-navigation__profile">
+          <i className="material-icons">person</i> Profile
+        </Link>
+        <Link to={`/privacy`} className="mdl-navigation__link mdl-navigation__privacy">
+          <i className="material-icons">info_outline</i> Privacy
+        </Link> 
+      </nav>
+
+      <hr />
+
+      <div className="mdl-navigation">
+        <p className="mdl-navigation__link">Copyright &copy; 2018</p>
+      </div> 
+    </div>
+  );
+}//[end] DrawerNav
 
 
 const MainTabs = () => {
@@ -77,8 +116,31 @@ const MainTabs = () => {
 }//[end] MainTabs
 
 
-const Sidebar = () => {
+const MainHeader = () => {
+  return(
+    <header className="app-header">
+      <h1 className="app-header__title">
+        Fanci <i className="material-icons">person_pin</i>
+      </h1>
 
+      <div className="app-header__search mdl-textfield mdl-js-textfield mdl-textfield--expandable mdl-textfield--floating-label mdl-textfield--align-right">
+        <label className="mdl-button mdl-js-button mdl-button--icon" htmlFor="fixed-header-drawer-exp">
+          <i className="material-icons">search</i>
+        </label>
+        <div className="mdl-textfield__expandable-holder">
+          <input className="mdl-textfield__input" type="text" name="sample" id="fixed-header-drawer-exp" />
+        </div>
+      </div>
+
+      <MainTabs />
+
+      <div className="mdl-layout-spacer"></div> 
+    </header>
+  );
+}//[end] MainHeader
+
+
+const Sidebar = () => {
   return(
     <aside>
       <ul>
